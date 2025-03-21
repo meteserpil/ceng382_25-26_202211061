e1c1b892eb754824915415ac2e9b5a3271333771
Mete Serpil <meteserpil@Metes-MacBook-Air.local>
2025-03-21 23:53:41 +0300

    Added this week's files

--- LabProject/obj/Debug/net9.0/LabProject.AssemblyInfo.cs
+++ LabProject/obj/Debug/net9.0/LabProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5900105c67aff6a83dc952b2ff1558eeacbfac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a24c552d25613e84a0af99118aae699a413e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
